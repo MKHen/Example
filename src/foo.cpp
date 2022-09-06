2f560dc29cd899b2a3c785043691e34aea03d3b2
MKHen <84001275+MKHen@users.noreply.github.com>
2022-09-06 06:34:33 +0200

    Create foo.cpp

---
+++ src/foo.cpp
@@ -0,0 +1,22 @@
+/** @file
+ *  @copyright ABC
+ */
+#include <foo.h>
+
+/**
+ * @brief Docu of foo init_2() function
+ * @li Comment 1.
+ * @return Always true
+ */
+bool foo::init_2() {
+    return true;
+}
+
+/**
+ * @brief Docu of foo example_2() function
+ * @li Comment to example_2.
+ * @return Always 7
+ */
+int16_t foo::example_2() {
+    return 77;
+}
